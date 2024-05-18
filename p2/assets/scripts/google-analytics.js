dbd79e17284ec8864482c377eaab723bab4cb516
eduardaleman <aleman@ealeman.com>
2024-05-17 17:31:23 -0700

    assets

---
+++ p2/assets/scripts/google-analytics.js
@@ -0,0 +1,6 @@
+<!-- Google tag (gtag.js) -->
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-B1BH7YYTER');
