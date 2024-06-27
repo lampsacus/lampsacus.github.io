61ca7c8d83b6955959c525d4b38beb012496209c
eduardaleman <aleman@ealeman.com>
2024-06-26 23:23:44 -0700

    newlandingpage

--- assets/scripts/google-analytics.js
+++
@@ -1,6 +0,0 @@
-<!-- Google tag (gtag.js) -->
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-B1BH7YYTER');
